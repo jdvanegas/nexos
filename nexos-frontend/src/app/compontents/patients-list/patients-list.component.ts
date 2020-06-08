7d45deddeb6399fa8f680efac107af53289fb8bc
Juan David Vanegas <jdvanegas4@gmail.com>
2020-06-07 21:54:17 -0500

    Delete action

--- nexos-frontend/src/app/compontents/patients-list/patients-list.component.ts
+++ nexos-frontend/src/app/compontents/patients-list/patients-list.component.ts
@@ -1,32 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { PatientService } from '../../services/patient.service'
+import { PatientService } from '../../services/patient.service';
 import { Patient } from 'src/app/models/patient';
 
 @Component({
   selector: 'app-patients-list',
   templateUrl: './patients-list.component.html',
-  styleUrls: ['./patients-list.component.scss']
+  styleUrls: ['./patients-list.component.scss'],
 })
 export class PatientsListComponent implements OnInit {
+  patientList: Patient[] = [];
 
-  patientList: Patient[] = []
-
-  constructor(
-    private patientService: PatientService
-  ) {
+  constructor(private patientService: PatientService) {
     this.getPatientsList();
-   }
-
-  ngOnInit(): void {
   }
 
-  getPatientsList(){
+  ngOnInit(): void {}
+
+  getPatientsList() {
     this.patientService.getPatients().subscribe(
-      (res: Patient[])=>{
-        this.patientList = res
-      }, err=> {
-        console.log(err)
-      })
+      (res: Patient[]) => {
+        this.patientList = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
+  deletePatient(id) {
+    this.patientService.deletePatient(id).subscribe(
+      (res) => {
+        this.getPatientsList();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
--- nexos-frontend/src/app/services/patient.service.ts
+++ nexos-frontend/src/app/services/patient.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { environment } from '../../environments/environment'
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { Patient } from '../models/patient';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PatientService {
+  URL_PATIENSTLIST = environment.url_patientsList;
 
-  URL_PATIENSTLIST = environment.url_patientsList
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-
-  getPatients(id = null){
-    return this.http.get<Patient[]>(`${this.URL_PATIENSTLIST}/${id ? id : ''}`)
+  getPatients(id = null) {
+    return this.http.get<Patient[]>(`${this.URL_PATIENSTLIST}/${id ? id : ''}`);
   }
 
-  createPatient(body){
-    return this.http.post<any>(this.URL_PATIENSTLIST, body)
+  createPatient(body) {
+    return this.http.post<any>(this.URL_PATIENSTLIST, body);
   }
 
-  updatePatient(body){
-    return this.http.put<any>(this.URL_PATIENSTLIST, body)
+  updatePatient(body) {
+    return this.http.put<any>(this.URL_PATIENSTLIST, body);
   }
 
+  deletePatient(id) {
+    return this.http.delete<any>(`${this.URL_PATIENSTLIST}/${id}`);
+  }
 }
